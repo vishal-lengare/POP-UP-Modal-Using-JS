ebb3aaf5bc90d30c1e2b645e809c51e6cc8bf6d1
vishal <vishallengare61@gmail.com>
2024-04-10 19:27:06 +0530

    create the pop up modal using JS

---
+++ app.js
@@ -0,0 +1,10 @@
+
+let popup = document.getElementById('popup');
+
+function openPopUp(){
+    popup.classList.add('open-popup');
+}
+
+function closePopUp(){
+    popup.classList.remove('open-popup');
+}
